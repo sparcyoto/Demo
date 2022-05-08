fdf19025ae5f1d7e3cdc24a84d8b3c7628b95bc2
shivam yadav <shivamy@tekion.com>
2022-05-08 18:17:54 +0530

    second commit for the dev branch

--- indes.js
+++ indes.js
@@ -3,3 +3,5 @@ console.log('hello world')
 console.log('name is something and want to commit to this change using the pull request')
 
 console.log('new dev branch is added')
+
+console.log('second commit on the dev branch')
